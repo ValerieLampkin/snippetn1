e41df8e6eb6cbbedfff67e2b94b0098d329c0804
Syahrul Aiman Shaharuddin <syahrul.aiman@my.ibm.com>
2017-01-11 14:41:14 +0800

    update package & snippet

--- ImageProcessing/VisualRecognition/src/snippet.js
+++ ImageProcessing/VisualRecognition/src/snippet.js
@@ -1,40 +1,37 @@
 var parameters = {
-  "apikey" : "",
-  "url" : "https://www.whitehouse.gov/sites/whitehouse.gov/files/images/first-family/44_barack_obama%5B1%5D.jpg" 
+		"apikey" : "",
+		"url" : "https://www.whitehouse.gov/sites/whitehouse.gov/files/images/first-family/44_barack_obama%5B1%5D.jpg" 
 };
 
 //Main function
 //Output will be reflected via console.log function
-function process(req_parameters, callback) {
+function handler(req_parameters, callback) {
 	var watson = require('watson-developer-cloud');
 	var fs = require('fs');
 	var http = require('http');
-	
-	var visual_recognition = watson.visual_recognition({
-	  api_key: req_parameters.apikey, //SET YOU API KEY
-	  version: 'v3',
-	  version_date: '2016-05-19'
-	});
 
-	visual_recognition.detectFaces(parameters, 
-		function (err, response) {
-		    if (err) {
-		      console.log('error:', err);
-		      if (typeof callback !== 'undefined' && typeof callback=="function") return callback(err);
-		    }
-		    else {
-		      console.log(JSON.stringify(response, null, 2));
-				if (typeof callback !== 'undefined' && typeof callback=="function") return callback(response);
-		    }
+	var visual_recognition = new watson.VisualRecognitionV3({
+		api_key: req_parameters.apikey, //SET YOUR API KEY
+		version_date: '2015-05-20'
+	});
+	
+	visual_recognition.detectFaces(parameters, (err, response) => {
+		if (err) {
+			console.log('error:', err);
+			if (typeof callback !== 'undefined' && typeof callback=="function") return callback(err);
 		}
-	);
+		else {
+			console.log(JSON.stringify(response, null, 2));
+			if (typeof callback !== 'undefined' && typeof callback=="function") return callback(response);
+		}
+	});
 }
 
 
-//Allows Execution of this process
+//Allows Execution of this handler
 //will run if only called directly
 if (require.main === module) {
-	process(parameters,null);
+	handler(parameters,null);
 } else {
 
 //	name of the unit for logging and servlet path also
@@ -42,7 +39,7 @@ if (require.main === module) {
 
 //	Template for making above code available
 //	as service via superglue routine
-	var superglue = require('../lib/superglue.js');
+	var superglue = require('sandbox-superglue');
 	module.exports = {
 			path: '/'+unitpath,
 			priority: 1,
@@ -51,6 +48,6 @@ if (require.main === module) {
 				// something to do initially
 			},
 			GET:  function(req, res) {superglue.GET(req,res,parameters,unitpath)},
-			POST: function(req, res) {superglue.POST(req,res,process)}
+			POST: function(req, res) {superglue.POST(req,res,handler)}
 	}
 }
